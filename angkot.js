1b29ba7166f4a0926df6a982bbc95e32c29523d0
Jodi Kris Antoni <jody201542@gmail.com>
2024-08-01 12:50:22 +0700

    "renew angkot js"

--- angkot.js
+++ angkot.js
@@ -1,13 +1,12 @@
 
 var jmlangkot = 10;
 var angkotberoperasi = 6;
-var angkotlembur = 8;
 
 
 for (var angkot = 1; angkot <= jmlangkot; angkot++) {
-    if (angkot <= angkotberoperasi){
+    if (angkot <= angkotberoperasi && angkot !==5){
         console.log ('angkot no' + angkot + 'beroperasi');
-    } else if (angkot === angkotlembur) {
+    } else if (angkot === 8 ||angkot === 10 || angkot === 5) {
         console.log ('angkot no' + angkot + 'lembur');
     } else {
         console.log ('angkot no' + angkot + 'tidak beroperasi');
