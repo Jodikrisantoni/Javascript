17a912cb488bb12bb410ec6e96ca64f79f79d559
Jodi Kris Antoni <jody201542@gmail.com>
2024-08-02 18:47:03 +0700

    making a simple refactoring

---
+++ refactoring.js
@@ -0,0 +1,17 @@
+// var volumeA;
+// var volumeB;
+// var total;
+
+// volumeA = a * a * a;
+// volumeB = b * b * b;
+
+// total = volumeA + volumeB;
+
+// return total;
+//diatas versi yang belum di refactoring
+
+function JumlahVolumeDuaKubus(a, b) {;
+    return a * a * a + b * b * b;
+}
+
+alert(JumlahVolumeDuaKubus(8, 3));
