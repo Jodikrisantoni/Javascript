5ad274c5e99ced58eccee359b9f59df6c8732f8f
Jodi Kris Antoni <jody201542@gmail.com>
2024-08-14 23:50:42 +0700

    add modif

--- Js DOM/DOMtravesal.js
+++ Js DOM/DOMtravesal.js
@@ -1,11 +1,9 @@
-
-
-
-
-
-
-const Close = document.querySelectorAll('.close');
-
+//all methode
+//parentNode
+//ParentElement
+//nextSibling
+//PreviousSibling
+//previousElementSibling
 // for(let i=0; i < close.length; i++){
 //     Close[i].addEventListener('click', function(e){
         
@@ -14,9 +12,12 @@ const Close = document.querySelectorAll('.close');
 // }
 // Close[i].parentElement.style.display = 'none';
 // e.target.parentElement.style.display = 'none';
+const Close = document.querySelectorAll('.close');
 
 Close.forEach(function (params) {
         params.addEventListener('click', function(e){
             e.target.parentElement.style.display = 'none';
         })
 });
+
+
