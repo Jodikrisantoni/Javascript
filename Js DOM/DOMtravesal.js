73ec6536227b3b28c011d708e82e35bc612c837b
Jodi Kris Antoni <jody201542@gmail.com>
2024-08-16 00:34:44 +0700

    preventDefault

--- Js DOM/DOMtravesal.js
+++ Js DOM/DOMtravesal.js
@@ -7,17 +7,22 @@
 // for(let i=0; i < close.length; i++){
 //     Close[i].addEventListener('click', function(e){
         
-
+//>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<
 //     });
 // }
 // Close[i].parentElement.style.display = 'none';
-// e.target.parentElement.style.display = 'none';
+// params.target.parentElement.style.display = 'none';
 const Close = document.querySelectorAll('.close');
 
 Close.forEach(function (params) {
         params.addEventListener('click', function(e){
             e.target.parentElement.style.display = 'none';
+            e.preventDefault();
         })
 });
 
+
+//prevent default untuk menghentikan aksi default element atau attribute
+//untuk menghentikan aksi default yang sudah tecipta dari
+//default dari element, 
 
