42d9679963f7d99e974cfaebc37d5d5e791365c0
Jodi Kris Antoni <jody201542@gmail.com>
2024-08-16 17:14:34 +0700

    gallery Home project

---
+++ Js DOM/galleryHOME.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+const mainpicture = document.querySelector('.cathead');
+const catpic = document.querySelector('.cat');
+
+container.addEventListener('click', function(e){
+    if(e.target.className == 'cat'){
+        mainpicture.src = e.target.src;
+        mainpicture.classList.add('fade');
+        setTimeout(function(){
+            mainpicture.classList.remove('fade')
+        }, 500);
+        
+        catpic.forEach(function(catcat){
+            if(catpic.classList.contains('active')){
+                catpic.classList.remove('active');
+            }       
+        });
+        
+        e.target.classList.add('active');
+    };
+});
