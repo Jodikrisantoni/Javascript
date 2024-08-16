21a238eb0b011e2f3157dec660a982e81e72b03c
Jodi Kris Antoni <jody201542@gmail.com>
2024-08-16 13:11:42 +0700

    add new note about bubble

--- Js DOM/DOMtravesal.js
+++ Js DOM/DOMtravesal.js
@@ -12,17 +12,37 @@
 // }
 // Close[i].parentElement.style.display = 'none';
 // params.target.parentElement.style.display = 'none';
-const Close = document.querySelectorAll('.close');
+// const Close = document.querySelectorAll('.close');
 
-Close.forEach(function (params) {
-        params.addEventListener('click', function(e){
-            e.target.parentElement.style.display = 'none';
-            e.preventDefault();
-        })
-});
+// Close.forEach(function (params) {
+//         params.addEventListener('click', function(e){
+//             e.target.parentElement.style.display = 'none';
+//             e.preventDefault();
+//             e.stopPropagation();
+//         })
+// });
 
+// const allcard = document.querySelectorAll('.card')
+// allcard.forEach(function(onecard)(
+//     onecard.addEventListener('click', function(){
+
+//     })
+// ))
 
 //prevent default untuk menghentikan aksi default element atau attribute
 //untuk menghentikan aksi default yang sudah tecipta dari
 //default dari element, 
 
+//event bubbling merupakan kejadian event dalam satu container
+//yang dimana setiap event akan dijlankan hingga terahir bubble
+//gunakan stopPropagation() agar bubble langsung berhenti
+
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function(event){
+    if (event.target.className == 'close'){
+        event.target.parentElement.style.display = 'none';
+        event.preventDefault();
+    }
+})
