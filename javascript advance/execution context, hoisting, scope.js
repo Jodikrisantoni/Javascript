89df90db9e299e74b1934d739af4865a4928ecc4
Jodi Kris Antoni <jody201542@gmail.com>
2024-08-27 22:49:44 +0700

    execution, hoisting and scope

---
+++ javascript advance/execution context, hoisting, scope.js
@@ -0,0 +1,54 @@
+// creation phase pada Global context
+// nama var = undefined
+// nama function = fn()
+// hoisting
+// window = global object 
+// this = window
+
+////////////////////////////////////////////////////////////
+// execution phase
+// maka hasilnya var tidak akan di eksekusio karna tidak tereksekusi
+// dari atas ke bawah
+
+// kasus 1
+// console.log(say());
+
+// var nama = 'jodi';
+// var umur = 33;
+
+// function say(){
+//     return `halo, nama saya ${nama}, saya ${umur} tahun`;
+// };
+
+
+// function akan membuat local execution context
+// yang didalammnya terdapat creation dan execution phase
+// bisa akses ke window dan arguments
+// terdapat hoisting local
+////////////////////////////////////////////////////////////
+
+// kasus 2
+// var nama = 'Jodi Kris Antoni';
+// var username = 'jody_krisantoni';
+
+// function cetakURL(username){
+//     var instagramURL = "http://instagram.com/";
+//     return instagramURL + username;
+
+// }
+
+// console.log(cetakURL(username));
+
+// kasus 3
+
+function a(){
+    console.log('ini a');
+
+    function b(){
+        console.log (' ini b');
+
+        function c (){
+            console.log('ini c')
+        }
+    }
+}
